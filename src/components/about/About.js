07bbcca64773aab117991d4c1849bbec390890ec
Allie Robinson <allierob93@gmail.com>
2020-09-15 18:55:13 -0500

    changed about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,12 +8,7 @@ const About = (props) => {
             <div className="about-txt">
                 <h2>Jon Robinson</h2>
                 <h3>Based in Tacoma, WA</h3>
-                Hi! Thanks for visiting my website. I love the Pacific Northwest and have spent my life
-developing roots here. My work brings me in contact with all kinds of people and all kinds of
-interesting work projects. At Jon the Builder, we love improving our neighbors’ lives by building with
-skill, precision, and integrity. Collectively, we have years of experience in the interior and exterior
-carpentry business and we pride ourselves on a job well done. If you have any questions about how we
-can help you, don’t hesitate to give us a call.
+                Thanks for taking the time to check out our website! I love the Pacific Northwest and have spent my life developing roots here. Our work brings us in contact with all kinds of people and all kinds of interesting projects. At Jon the Builder, we love improving our neighbors’ lives by building with skill, precision, and integrity. Collectively, we have years of experience in the interior and exterior carpentry business and we pride ourselves on a job well done. If you have any questions about how we can help you, don’t hesitate to give us a call.
                 <a href="mailto:info@jonthebuilderllc.com"><button className="contact-btn">Contact</button></a>
                 </div>
         </div>
